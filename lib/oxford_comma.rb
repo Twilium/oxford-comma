765dd5cb798320791fbc7c16615b42dffa327799
Emmanuel <rivera128@live.com>
2020-10-27 20:53:01 -0500

    commit tree 731810d08219edd7a3d0fcf89c88da7a4ffeb9a8

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    if array.size == 1 
+        array.join
+    elsif array.size == 2
+        new_array = array.to_a
+        new_array.join(" and ")
+    elsif array.size == 3
+        new_array = array.to_a
+        new_array[0..1].join(", ") + ", and " + new_array[-1]
+    else
+        new_array = array.to_a
+        new_array[0..-2].join(", ") + ", and " + new_array[-1]
+    end
 end
